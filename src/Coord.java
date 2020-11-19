966c26ffef63d5051e110cef1168d942aa13f4d9
tpennerat <theo.pennerat@gmail.com>
2020-11-19 13:55:31 +0100

    Classe Coord et début des inputs users dans le main

--- src/Coord.java
+++ src/Coord.java
@@ -1,2 +1,50 @@
+/**
+ * Classe qui définit les coordonnées d'une carte à travers une Map
+ * @see AbstractBoard
+ */
 public class Coord {
+    private int posX;
+    private int posY;
+
+    /**
+     * Constructeur de la classe Coord
+     * @param x entier sur l'axe x
+     * @param y entier sur l'axe y
+     */
+    public Coord(int x, int y) {
+        posX = x;
+        posY = y;
+    }
+
+    /**
+     * getter de l'attribut posX
+     * @return l'attribut posX
+     */
+    public int getPosX() {
+        return posX;
+    }
+
+    /**
+     * setter de l'attribut posX
+     * @param posX l'entier qui va remplacer posX
+     */
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    /**
+     * getter de l'attribut posY
+     * @return l'attribut posY
+     */
+    public int getPosY() {
+        return posY;
+    }
+
+    /**
+     * setter de l'attribut posY
+     * @param posY l'entier qui va remplacer posY
+     */
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
 }
--- src/ShapeUp.java
+++ src/ShapeUp.java
@@ -1,5 +1,41 @@
-public class ShapeUp {
-    public static void main(String[] args) {
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
+public class ShapeUp {
+    private List<Player> playerList;
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenue dans ShapeUp !"); // salutation du joueur
+        // demande le nombre de joueur à l'utilisateur
+        String messagePlayer = "À combien de joueur souhaites-tu jouer ?";
+        int nbPlayer = askNumber(messagePlayer);
+        while (nbPlayer > 3 || nbPlayer < 2) {
+            System.err.println("Le nombre de joueur doit être 2 ou 3.");
+            nbPlayer = askNumber(messagePlayer);
+        }
+        // demande le nombre de joueur réel à l'utilisateur
+        String messageRealPlayer = "Combien y a-t-il de joueur réel parmis les " + nbPlayer + " joueurs ?";
+        int nbRealPlayer = askNumber(messageRealPlayer);
+        while (nbRealPlayer > nbPlayer || nbRealPlayer < 1) {
+            System.err.println("Le nombre de joueur doit être compris entre 1 et " + nbPlayer + ".");
+            nbRealPlayer = askNumber(messageRealPlayer);
+        }
+
+    }
+
+    private static int askNumber(String message) {
+        System.out.println(message);
+        Scanner sc = new Scanner(System.in);
+        int res;
+        try {
+            res = sc.nextInt();
+        } catch (InputMismatchException ime) {
+            System.err.println("Attention, ton nombre n'est pas un entier.");
+            sc.nextLine();
+            return -1;
+        }
+        sc.nextLine();
+        return res;
     }
 }
