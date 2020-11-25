623187d3e9f673de749622c3991427b65b57dc18
Brendsig <brendsig40@gmail.com>
2020-11-25 17:30:13 +0100

    Moodifs PlayingStrategy

--- src/PlayingStrategy1.java
+++ src/PlayingStrategy1.java
@@ -1,22 +1,18 @@
 public class PlayingStrategy1 implements PlayingStrategy {
 	
 	@Override
-	public playVirtual() {
+	public Coord playVirtual() {
         int Min = -10;
 		int Max = 10;
 		
 		int randomX = Min + (int)(Math.random() * ((Max - Min) + 1));
 		int randomY = Min + (int)(Math.random() * ((Max - Min) + 1));
 		
-		while (randomX == null || randomY == null)  {
-             randomX = Min + (int)(Math.random() * ((Max - Min) + 1));
-             randomY = Min + (int)(Math.random() * ((Max - Min) + 1));
-        }
         return new Coord(randomX,randomY);
     }
 	
 	@Override
-	public moveVirtual() {
+	public Coord moveVirtual() {
         
 		
 		
