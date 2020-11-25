79ff7c22dff2b424e084b5580513590839581a24
Brendsig <brendsig40@gmail.com>
2020-11-25 17:17:11 +0100

    modifs methode play pour virtualplayer (ptovisoire)

--- src/PlayingStrategy1.java
+++ src/PlayingStrategy1.java
@@ -1,2 +1,25 @@
 public class PlayingStrategy1 implements PlayingStrategy {
+	
+	@Override
+	public playVirtual() {
+        int Min = -10;
+		int Max = 10;
+		
+		int randomX = Min + (int)(Math.random() * ((Max - Min) + 1));
+		int randomY = Min + (int)(Math.random() * ((Max - Min) + 1));
+		
+		while (randomX == null || randomY == null)  {
+             randomX = Min + (int)(Math.random() * ((Max - Min) + 1));
+             randomY = Min + (int)(Math.random() * ((Max - Min) + 1));
+        }
+        return new Coord(randomX,randomY);
+    }
+	
+	@Override
+	public moveVirtual() {
+        
+		
+		
+		return null;
+    }
 }
