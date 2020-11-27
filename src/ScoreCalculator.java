2e43dfb87082b09859e5f5fbe05a0be09981009c
Brendsig <brendsig40@gmail.com>
2020-11-27 11:22:40 +0100

    moodifs ScoreCalculator (pas finie)

--- src/ScoreCalculator.java
+++ src/ScoreCalculator.java
@@ -8,29 +8,92 @@ public class ScoreCalculator implements InterfaceVisitor {
 		Shape victoryshape = victorycard.getShape();
 		boolean victoryfilled = victorycard.isFilled();
 
-		//Parcourir plateau et voir pour toutes les cartes adjacentes si elles ont des caractéristiques communes
-		for (Card card : placedCards) { 
+		int minX = abstractboard.getRealMinimunX(); //Récupére les bornes du plateau
+		int maxX = abstractboard.getRealMaximumX();
+		int minY = abstractboard.getRealMinimunY();
+		int maxY = abstractboard.getRealMaximumY();
+
+		//-------------------------------------- initialisation ----------------------------------------
+		
+		int posX = minX; 
+		int posY = minY;
+		
+		card.setPosX(minX); //Pointe sur la 1ere carte du plateau
+		card.setPosY(minY);
+
+		card.hashCode(); //retourne la carte en (minX,minY)
+		
+		Color cardcolor = card.getColor();  //Récupère les attributs de la card en (minX,minY)
+		Shape cardshape = card.getShape();
+		boolean cardfilled = card.isFilled();
+
+		int i = 0; //compte le nombre de cartes identiques
+		int cpt_int = 0; //compte le nombre de points en fonction du nombre de cartes identiques
+		int cpt_tot = 0; //compte le nombre de points en fonction du nombre de cartes identiques
+
+		//-----------------------------------------------------------------------------------------------
+
+		while((posX < maxX) && (posY < maxY)) {
 			
-			Color cardcolor = card.getColor(); 
-			Shape cardshape = card.getShape();
-			boolean cardfilled = card.isFilled();
-
-			if (coord.isCoordAdjacent(Coord c) == true) {  //
-				if (cardcolor == victorycolor) {
-
+			while(cardcolor == victorycolor) { //tant que color commune on continue a scanner la prochaine carte en x
+				posX++; i++;
+				card.setPosX(posX);
+				card.hashCode(); //retourne la carte en (posX,minY)
+				Color cardcolor = card.getColor(); //recupere cardcolor de la nouvelle carte
+	
+				if(i >= 3){ //si au moins 3 cartes avec meme couleur, augmentez cpt_int
+					cpt_int = i+1;
 				}
-				if (cardshape == victoryshape) {
-					
+	
+				if(!(cardcolor == victorycolor)) { //si la carte n'a plus la bonne couleur
+					posX++; 
+					i = 0;
+					cpt_tot += cpt_int;
+					cpt_int = 0;
 				}
-				if (cardfilled == victoryfilled) {
-					
+				
+			}
+			
+			while(cardshape == victoryshape) { //tant que shape commune on continue a scanner la prochaine carte en x
+				posX++; i++;
+				card.setPosX(posX);
+				card.hashCode(); //retourne la carte en (posX,minY)
+				Color cardshape = card.getShape(); 
+	
+				if(i >= 2){ //si au moins 2 cartes avec meme shape, augmentez cpt_int
+					cpt_int = i-1;
+				}
+	
+				if(!(cardshape == victoryshape)) { //si la carte n'a plus la bonne shape
+					posX++; 
+					i = 0;
+					cpt_tot += cpt_int;
+					cpt_int = 0;
 				}
 			}
-
+			
+			while(cardfilled== victoryfilled) { //tant que filled commune on continue a scanner la prochaine carte en x
+				posX++; i++;
+				card.setPosX(posX);
+				card.hashCode(); //retourne la carte en (posX,minY)
+				Color cardfilled = card.isFilled(); 
+	
+				if(i >= 3){ //si au moins 3 cartes avec meme filled, augmentez cpt_int
+					cpt_int = i;
+				}
+				
+				if(!(cardfilled == victoryfilled)) { //si la carte n'a plus le bon filled
+					posX++; 
+					i = 0;
+					cpt_tot += cpt_int;
+					cpt_int = 0;
+				}
+			}
 		}
-
-
 		
+		//for (Card card : placedCards) { }
+
+		return cpt_tot;
 	}
 	
 }
